3d5301ddfd85f92c23e93a2308a85d9a1ad4f037
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2022-04-07 18:43:05 -0300

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,7 +1,9 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
+#end
 
 using StringTools;
 
@@ -9,6 +11,7 @@ class DiscordClient
 {
 	public function new()
 	{
+		#if desktop
 		trace("Discord Client starting...");
 		DiscordRpc.start({
 			clientID: "885207099322290226",
@@ -26,21 +29,26 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+		#end
 	}
 
 	public static function shutdown()
 	{
+		#if desktop
 		DiscordRpc.shutdown();
+		#end
 	}
 
 	static function onReady()
 	{
+		#if desktop
 		DiscordRpc.presence({
 			details: "Getting Trolled",
 			state: null,
 			largeImageKey: 'icon',
 			largeImageText: "Tails Gets Trolled"
 		});
+		#end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -55,15 +63,18 @@ class DiscordClient
 
 	public static function initialize()
 	{
+		#if desktop
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+		#end
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+		#if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -83,5 +94,6 @@ class DiscordClient
 		});
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
+		#end
 	}
 }
